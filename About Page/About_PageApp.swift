a7f934248f071f15baa16c2f01ecbb4460a2d1cc
ZiXiang <wzxiang9301@gmail.com>
2024-09-25 10:49:51 +0800

    Initial Commit

---
+++ About Page/About_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_PageApp.swift
+//  About Page
+//
+//  Created by 翁子翔 on 2024/9/25.
+//
+
+import SwiftUI
+
+@main
+struct About_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About PageTests/About_PageTests.swift
@@ -0,0 +1,17 @@
+//
+//  About_PageTests.swift
+//  About PageTests
+//
+//  Created by 翁子翔 on 2024/9/25.
+//
+
+import Testing
+@testable import About_Page
+
+struct About_PageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
